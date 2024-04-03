3dc47ff490d773e8f80dec44764b8e8321b5e92e
Emiresk <svv.luga@gmail.com>
2024-04-03 22:51:39 +0300

    Updated AppPalette

--- lib/Application/Style/Palette.dart
+++ lib/Application/Style/Palette.dart
@@ -9,18 +9,17 @@ https://www.schemecolor.com/search/Car/page/2
 
 
 class AppPalette {
-  static Color RED = const Color(0xffD8280B);
 
-  static Color BLACK = const Color (0xff1A1E21);
+  AppPalette._();
 
-  static Color WHITE = Color (0xffF7FBFC);
-
-  static Color GRAY100 = const Color (0xff6E7475);
-  static Color GRAY200 = const Color (0xff93999B);
-  static Color GRAY300 = const Color (0xffBEC5C7);
-
-  static Color ORANGE = const Color (0xffFBC368);
-  static Color YELLOW = const Color (0xffE8BF3A);
-  static Color GREEN = const Color (0xff2BA136);
+  static const Color RED = Color(0xffD8280B);
+  static const Color BLACK =  Color (0xff1A1E21);
+  static const Color WHITE = Color (0xffF7FBFC);
+  static const Color GRAY100 = Color (0xff6E7475);
+  static const Color GRAY200 = Color (0xff93999B);
+  static const Color GRAY300 = Color (0xffBEC5C7);
+  static const Color ORANGE = Color (0xffFBC368);
+  static const Color YELLOW = Color (0xffE8BF3A);
+  static const Color GREEN = Color (0xff2BA136);
 
 }

a0c1ef58213c21a67af12b84863c68ed5edf38d7
Emiresk <svv.luga@gmail.com>
2024-04-03 22:47:24 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:proto/Application/Providers/AppStateProvider.dart';
+import 'package:proto/Application/Application.dart';
+
+
+
+
 
 void main() {
-  runApp(const MainApp());
-}
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AppStateProvider() ),
+      ],
+      child: Application(),
+    )
+  );
 }

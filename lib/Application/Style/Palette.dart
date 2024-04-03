8a44c95ad99cda55b2bdbfe66622f2fab0c798d3
Emiresk <svv.luga@gmail.com>
2024-04-03 22:46:56 +0300

    Implement AppPalette
    It's a application colors

---
+++ lib/Application/Style/Palette.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:flutter/material.dart';
+
+/*
+Source :
+https://www.schemecolor.com/search/Car/page/2
+ */
+
+
+class AppPalette {
+  static Color RED = const Color(0xffD8280B);
+
+  static Color BLACK = const Color (0xff1A1E21);
+
+  static Color WHITE = Color (0xffF7FBFC);
+
+  static Color GRAY100 = const Color (0xff6E7475);
+  static Color GRAY200 = const Color (0xff93999B);
+  static Color GRAY300 = const Color (0xffBEC5C7);
+
+  static Color ORANGE = const Color (0xffFBC368);
+  static Color YELLOW = const Color (0xffE8BF3A);
+  static Color GREEN = const Color (0xff2BA136);
+
+}

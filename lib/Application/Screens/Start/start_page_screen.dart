2a01a84294b21e914db4266b1512b1841843629f
Emiresk <svv.luga@gmail.com>
2024-04-14 16:41:26 +0300

    Added component for test

--- lib/Application/Screens/Start/start_page_screen.dart
+++ lib/Application/Screens/Start/start_page_screen.dart
@@ -1,4 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+
+import 'package:proto/langs/locale_keys.g.dart';
 
 class StartPageScreen extends StatefulWidget {
    @override
@@ -16,12 +19,36 @@ class EStartPageScreenState extends State<StartPageScreen> {
             const SizedBox( height: 10.0 ),
           ],
         ),
-        drawer: const Drawer(),
-        body: const Column(
+
+        body: Column(
           children: [
             Center(
-              child: Text ("Hi"),
-            )
+              child: Text (LocaleKeys.startpage_header.tr() + " " + LocaleKeys.startpage_subheader.tr()),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    print( "Selected russian");
+                    context.setLocale( Locale('ru'));
+                  },
+                  child: const Text('Russian'),
+                ),
+
+                ElevatedButton(
+                  onPressed: () => context.setLocale( Locale('en')),
+                  child: const Text('England'),
+                ),
+
+                ElevatedButton(
+                  onPressed: () => context.setLocale( Locale('ua')),
+                  child: const Text('Ukrainian'),
+                ),
+              ],
+            ),
           ],
         ),
       ),

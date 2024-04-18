514361f603a4e302b4165ad09411c35390164bbd
SilentAI <svv.luga@gmail.com>
2024-04-19 00:07:30 +0300

    Deleted old and unused files

--- lib/langs/locale_keys.g.dart
+++
@@ -1,7 +0,0 @@
-// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
-
-abstract class  LocaleKeys {
-  static const startpage_header = 'startpage_header';
-  static const startpage_subheader = 'startpage_subheader';
-
-}

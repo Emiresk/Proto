7b04359b4e4606467370428e5d26239044c87033
Emiresk <svv.luga@gmail.com>
2024-04-20 01:02:59 +0300

    Auto generated assets

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -5,10 +5,13 @@ class Assets {
   static const String applicationApplication = 'lib/Application/Application.dart';
   static const String configLocalizationConfig = 'lib/Application/Config/LocalizationConfig.dart';
   static const String generatedAssets = 'lib/generated/assets.dart';
+  static const String langCodegenLoader.g = 'lib/lang/codegen_loader.g.dart';
+  static const String langLocaleKeys.g = 'lib/lang/locale_keys.g.dart';
   static const String libMain = 'lib/main.dart';
   static const String providersAppStateProvider = 'lib/Application/Providers/AppStateProvider.dart';
   static const String providersSplashScreenProvider = 'lib/Application/Providers/SplashScreenProvider.dart';
   static const String routerRouter = 'lib/Application/Router/Router.dart';
+  static const String settingsSettingsPageScreen = 'lib/Application/Screens/Settings/settings_page_screen.dart';
   static const String splashSpashScreen = 'lib/Application/Screens/Splash/SpashScreen.dart';
   static const String startStartPageScreen = 'lib/Application/Screens/Start/start_page_screen.dart';
   static const String stylePalette = 'lib/Application/Style/Palette.dart';

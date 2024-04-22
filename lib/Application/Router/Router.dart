cbe59030445775af6b5d6a0acc3641a992f22ed0
Emiresk <svv.luga@gmail.com>
2024-04-22 22:42:17 +0300

    Fix router path's

--- lib/Application/Router/Router.dart
+++ lib/Application/Router/Router.dart
@@ -1,42 +1,33 @@
-
 import 'package:go_router/go_router.dart';
 
-import 'package:proto/Application/Providers/SplashScreenProvider.dart';
-import 'package:proto/Application/Screens/Splash/SpashScreen.dart';
 import 'package:proto/Application/Screens/Start/start_page_screen.dart';
 import 'package:proto/Application/Screens/Settings/settings_page_screen.dart';
+import 'package:proto/Application/Screens/Splash/splash_screen_page.dart';
 
-GoRouter CreateAppRouter ( SplashScreenProvider ssProvider ) => GoRouter(
+
+GoRouter CreateAppRouter () => GoRouter(
+
   debugLogDiagnostics: true,
-  initialLocation: '/',
 
-  refreshListenable: ssProvider,
-
-  //observers: '',
-
-  errorBuilder: ( context, state ) {
-    if ( ssProvider.showSplashOnStart() ){
-      return SplashScreen();
-    }
-
-    else {
-      return StartPageScreen();
-    }
-  },
+  initialLocation: '/splash',
 
   routes: [
+    GoRoute(
+      path: '/splash',
+      builder: (context, state ) => SplashScreen(),
+      name: 'Splash screen page',
+    ),
+
     GoRoute(
         path: '/start',
         builder: (context, state ) => StartPageScreen(),
         name: 'Main page',
-
     ),
 
     GoRoute(
         path: '/settings',
         builder: (context, state ) => SettingsPageScreen(),
         name: 'Settings page',
-
     ),
   ]
 );

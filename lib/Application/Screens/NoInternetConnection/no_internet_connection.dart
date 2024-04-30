ae54112107fcdb80d5d642d710137b1df95e8b6e
Emiresk <svv.luga@gmail.com>
2024-04-30 21:04:04 +0300

    Implement NoInternetScreenPage

--- lib/Application/Screens/NoInternetConnection/no_internet_connection.dart
+++ lib/Application/Screens/NoInternetConnection/no_internet_connection.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:proto/Application/Style/Palette.dart';
+
 class NoInternetConnection extends StatefulWidget {
   const NoInternetConnection({Key? key}) : super(key: key);
 
@@ -10,42 +12,60 @@ class NoInternetConnection extends StatefulWidget {
 class _NoInternetConnectionState extends State<NoInternetConnection> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          elevation: 1,
-          title: Text('No internet...'),
-        ),
-
-        body: const Column(
+        body: Column(
           children: [
-            SizedBox(height: 20,),
+            SizedBox(height: 50,),
             Center(
-              child:
-              Text (
-                'No internet connection...'
+              child: Text (
+                'Something went wrong...',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600
+                ),
               ),
             ),
             SizedBox(
-              height: 20,
+              height: 50,
             ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-              children: [
-                Icon( size: 50, Icons.signal_wifi_statusbar_connected_no_internet_4 )
-              ],
-            ),
-            SizedBox(height: 50,),
 
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('Checking internet connection ...')
+
+              children: [
+                Icon( size: 120, Icons.signal_wifi_statusbar_connected_no_internet_4 )
               ],
             ),
+
+            SizedBox(height: 50,),
+
+            Text('Internet connection has been lost',
+              style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400
+              ),
+            ),
+
+            SizedBox(height: 50,),
+
+            CircularProgressIndicator(
+              color: AppPalette.BLACK,
+              backgroundColor: AppPalette.GRAY300,
+            ),
+            SizedBox(height: 10,),
+
+            Text (
+              'Tryint to reconnect...',
+              style: TextStyle(
+                  fontSize: 10,
+                  fontWeight: FontWeight.w400
+              ),
+            ),
+
+
+
           ],
         ),
       ),

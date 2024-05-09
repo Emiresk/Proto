81d66f8db34c04f5dccaff3970f96575af73de24
Emiresk <svv.luga@gmail.com>
2024-05-10 02:19:04 +0300

    Added new colors

--- lib/Application/Style/Palette.dart
+++ lib/Application/Style/Palette.dart
@@ -22,4 +22,37 @@ class AppPalette {
   static const Color YELLOW = Color (0xffE8BF3A);
   static const Color GREEN = Color (0xff2BA136);
 
+
+  static const Color PRIMARY = Color (0xFF00296B);
+  static const Color SECONDARY = Color (0xFF5C5C95);
+  static const Color TERTIARY = Color (0xFFD26900);
+  static const Color ERROR = Color (0xFFBA1A1A);
+
+  static const Color PRIMARY_CONTAINER = Color (0xFFA0C2ED);
+  static const Color SECONDARY_CONTAINER = Color (0xFFC8DBF8);
+  static const Color TERTIARY_CONTAINER  = Color (0xFFFFD270);
+  static const Color ERROR_CONTAINER  = Color (0xFFFFDAD6);
+
+  static const Color onPRIMARY = Color (0xFFFFFFFF);
+  static const Color onSECONDARY = Color (0xFFFFFFFF);
+  static const Color onTERTIARY = Color (0xFFFFFFFF);
+  static const Color onERROR = Color (0xFFFFFFFF);
+
+  static const Color onPRIMARY_CONTAINER  = Color (0xFF0E1014);
+  static const Color onSECONDARY_CONTAINER  = Color (0xFF111214);
+  static const Color onTERTIARY_CONTAINER  = Color (0xFF14120A);
+  static const Color onERROR_CONTAINER  = Color (0xFF410002);
+
+  static const Color BUTTON_ELEVATED = Color (0xFFF8F9FA);
+  static const Color BUTTON_ELEVATED_FILL = Color (0xFF00296B);
+  static const Color BUTTON_TEXT = Color (0xFFF8F9FA);
+  static const Color BUTTON_TOGGLE = Color (0xFF00296B);
+  static const Color BUTTON_RADIO = Color (0xFF00296B);
+  static const Color BUTTON_TOOLTIP = Color (0xFF091B39);
+  static const Color DISABLED = Color (0x6100102A);
+  static const Color SHADOW = Color (0xFF000000);
+  static const Color FOCUS = Color (0x1F001C4A);
+  static const Color SPLASH = Color (0x1F001A45);
+
+
 }

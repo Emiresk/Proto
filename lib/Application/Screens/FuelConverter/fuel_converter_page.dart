25ed43ee93d85dc6d35540c019d3c3d4884ce19e
SilentAI <svv.luga@gmail.com>
2024-05-22 17:01:36 +0300

    Template design

--- lib/Application/Screens/FuelConverter/fuel_converter_page.dart
+++ lib/Application/Screens/FuelConverter/fuel_converter_page.dart
@@ -1,3 +1,5 @@
+
+
 import 'dart:ui';
 
 import 'package:easy_localization/easy_localization.dart';
@@ -10,8 +12,6 @@ import 'package:go_router/go_router.dart';
 import 'package:uicons/uicons.dart';
 
 import '../../Router/GoRouterPathCollector.dart';
-
-import 'package:radio_group_v2/radio_group_v2.dart';
 
 class FuelConverterPage extends StatefulWidget {
   const FuelConverterPage({Key? key}) : super(key: key);
@@ -28,14 +28,19 @@ class _FuelConverterPageState extends State<FuelConverterPage> {
 
   String _type = '';
 
+  String _dropdownType = '';
   List<String> _listFuel = ["US MPG", "UK MPG", "L/100 Km"];
 
   @override
   Widget build(BuildContext context) {
+
+    _dropdownType = _listFuel.first;
+
     return SafeArea(
       child: Scaffold(
         key: _scaffoldKey,
         appBar: AppBar(
+          backgroundColor: AppPalette.PRIMARY,
           actions: [
             IconButton(
               onPressed: () => context.go('/settings'),
@@ -47,6 +52,7 @@ class _FuelConverterPageState extends State<FuelConverterPage> {
             onPressed: () => _scaffoldKey.currentState?.openDrawer(),
             icon: Icon(UIcons.regularRounded.menu_burger),
           ),
+
         ),
 
         drawer: Drawer(
@@ -61,91 +67,105 @@ class _FuelConverterPageState extends State<FuelConverterPage> {
           ),
         ),
 
-        body: Column(
+        body: Stack (
           children: [
-            const SizedBox(height: 20,),
-            const Center(
-              child: Text (
-                "Fuel Converter",
-                  style: TextStyle(
-                      color: AppPalette.PRIMARY,
-                      fontSize: 35,
-                      fontWeight: FontWeight.w500),
+            Container (
+              height: 150,
+              decoration: const BoxDecoration(
+                color: AppPalette.PRIMARY,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(15),
+                  bottomLeft: Radius.circular(15)
+                )
               ),
             ),
-            const SizedBox( height: 20,),
             Container(
-              margin: EdgeInsets.only(left: 10, right: 10),
+              margin: EdgeInsets.only( top: 25, left: 15,right: 15),
+              height: 350,
               width: double.infinity,
               decoration: const BoxDecoration(
-                color: AppPalette.SECONDARY_CONTAINER,
+                color: AppPalette.WHITE,
                 borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                boxShadow: [ BoxShadow(
-                  color: AppPalette.SHADOW,
-                  blurRadius: 10,
-                  blurStyle: BlurStyle.outer,
-                  offset: Offset(0, 5),
-                  spreadRadius: -5
-                )],
+                  boxShadow: [ BoxShadow(
+                    color: AppPalette.SHADOW,
+                    blurRadius: 10,
+                    blurStyle: BlurStyle.outer,
+                    offset: Offset(0, 5),
+                    spreadRadius: -5
+                  )],
               ),
+
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 20,),
-                  const Text (
-                    "Select source value",
-                    style: TextStyle(
+
+              children: [
+                const SizedBox( height: 25,),
+                const Text ("Select source value",
+                  style: TextStyle(
+                  color: AppPalette.PRIMARY,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500),
+                ),
+                const SizedBox( height: 35,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    DropdownButtonHideUnderline(
+                      child: DropdownButton<String>(
+                        value: _dropdownType,
+                        onChanged: (String? newFuelType) => _dropdownType = newFuelType!,
+                        items: _listFuel.map<DropdownMenuItem<String>>((String value){
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Container(
+                              height: 50, // высота элементов списка
+                              alignment: Alignment.centerLeft,
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              child: Text(value),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(5))
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                        dropdownColor: Colors.white, // цвет фона выпадающего меню
+                        style: TextStyle(color: Colors.black, fontSize: 18), // стиль текста элементов
+                        icon: Icon(Icons.arrow_drop_down, color: Colors.black), // иконка раскрытия
+                        iconSize: 30, // размер иконки
+                      )
+                    ),
+                    
+                    DropdownMenu<String>(
+                      initialSelection: _listFuel[0],
+                      onSelected: (val) => _type = val!,
+                      dropdownMenuEntries: _listFuel.map<DropdownMenuEntry<String>>((String index){
+                        return DropdownMenuEntry<String>(
+                          value: index,
+                          label: '$index'
+                        );
+                      }).toList(),
+
+                    ),
+                    const Text (" TO ",
+                      style: TextStyle(
                         color: AppPalette.PRIMARY,
-                        fontSize: 25,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox( height: 20,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      DropdownMenu<String>(
-                        initialSelection: _listFuel[0],
-                        onSelected: (val) => setState(() {
-                          _type = val!;
-                        }),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400),
+                    ),
+                    DropdownMenu<String>(
+                      initialSelection: _listFuel[0],
+                      onSelected: (val) => _type = val!,
 
-                        dropdownMenuEntries: _listFuel.map<DropdownMenuEntry<String>>((String index){
-                          return DropdownMenuEntry<String>(
-                              value: index,
-                              label: '$index'
-                          );
-                        }).toList(),
-
-                      ),
-                      const Text (
-                        " TO ",
-                        style: TextStyle(
-                            color: AppPalette.PRIMARY,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400),
-                      ),
-                      DropdownMenu<String>(
-                        initialSelection: _listFuel[0],
-                        onSelected: (val) => setState(() {
-                          _type = val!;
-                        }),
-
-                        dropdownMenuEntries: _listFuel.map<DropdownMenuEntry<String>>((String index){
-                          return DropdownMenuEntry<String>(
-                              value: index,
-                              label: '$index'
-                          );
-                        }).toList(),
-                      ),
-                    ],
-                  ),
-                  const SizedBox( height: 30,),
-                  Container(
-                      height: 200,
-                      child: Text("---"),
-                  )
-                ],
-              ),
+                      dropdownMenuEntries: _listFuel.map<DropdownMenuEntry<String>>((String index){
+                        return DropdownMenuEntry<String>(
+                          value: index,
+                          label: '$index'
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
             ),
           ],
         ),
